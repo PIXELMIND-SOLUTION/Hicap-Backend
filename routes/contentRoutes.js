cd4a0e223e8e23159bc5edf144dee61f1b60ccc6
ganapathi9191 <ganapathivaraprasad123@gmail.com>
2025-07-21 11:48:21 +0530

    first3

--- routes/contentRoutes.js
+++ routes/contentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../middleware/multer');
+const upload = require('../utils/uploadMiddleware');
 const {
   createContent,
   getAllContent,
